7815d9696e661bd2d5b971d7429d081d190f2d26
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-15 11:36:39 +0800

    added service modules

---
+++ src/services/apiRestaurant.js
@@ -0,0 +1,55 @@
+const API_URL = "https://react-fast-pizza-api.jonas.io/api";
+
+const getMenu = async () => {
+	const res = await fetch(`${API_URL}/menu`);
+	if (!res.ok) throw Error("Failed getting menu");
+
+	const { data } = await res.json();
+
+	return data;
+};
+
+const getOrder = async (id) => {
+	const res = await fetch(`${API_URL}/order/${id}`);
+	if (!res.ok) throw Error(`Couldn't find order #${id}`);
+
+	const { data } = await res.json();
+
+	return data;
+};
+
+const createOrder = async (newOrder) => {
+	try {
+		const res = await fetch(`${API_URL}/order`, {
+			method: "POST",
+			body: JSON.stringify(newOrder),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		if (!res.ok) throw Error();
+
+		const { data } = await res.json();
+
+		return data;
+	} catch {
+		throw Error("Failed creating your order");
+	}
+};
+
+const updateOrder = async (id, updateObj) => {
+	try {
+		const res = await fetch(`${API_URL}/order/${id}`, {
+			method: "PATCH",
+			body: JSON.stringify(updateObj),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		if (!res.ok) throw Error();
+	} catch (err) {
+		throw Error("Failed updating your order");
+	}
+};
+
+export { getMenu, getOrder, createOrder, updateOrder };
