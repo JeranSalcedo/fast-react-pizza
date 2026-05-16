6cc5288c3d6501388146d26ebc3cf639361dfe6f
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-16 14:05:19 +0800

    added reusable buttons

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,18 @@
-const Button = ({ children, disabled }) => {
+import { Link } from "react-router-dom";
+
+const Button = ({ children, disabled, to }) => {
+	const className =
+		"inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+
+	if (to)
+		return (
+			<Link className={className} to={to}>
+				{children}
+			</Link>
+		);
+
 	return (
-		<div
-			className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4"
-			disabled={disabled}
-		>
+		<div className={className} disabled={disabled}>
 			{children}
 		</div>
 	);
