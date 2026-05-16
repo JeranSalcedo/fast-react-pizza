55611abe882f69395a7117412a908f1b266df343
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-16 15:10:19 +0800

    added menu styling

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
 	const menu = useLoaderData();
 
 	return (
-		<ul>
+		<ul className="divide-y divide-stone-200 px-2">
 			{menu.map((pizza) => (
 				<MenuItem key={pizza.id} pizza={pizza} />
 			))}
