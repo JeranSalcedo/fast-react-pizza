9807853bb5f14536941112151130ac184340c42a
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-16 08:29:52 +0800

    added order fetching

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,45 +1,12 @@
 // Test ID: IIDSAT
 
+import { useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiRestaurant";
 import {
 	remainingMinutes,
 	formatCurrency,
 	formatDate,
 } from "../../utils/helpers";
-
-const order = {
-	id: "ABCDEF",
-	customer: "Jonas",
-	phone: "123456789",
-	address: "Arroios, Lisbon , Portugal",
-	priority: true,
-	estimatedDelivery: "2027-04-25T10:00:00",
-	cart: [
-		{
-			pizzaId: 7,
-			name: "Napoli",
-			quantity: 3,
-			unitPrice: 16,
-			totalPrice: 48,
-		},
-		{
-			pizzaId: 5,
-			name: "Diavola",
-			quantity: 2,
-			unitPrice: 16,
-			totalPrice: 32,
-		},
-		{
-			pizzaId: 3,
-			name: "Romana",
-			quantity: 1,
-			unitPrice: 15,
-			totalPrice: 15,
-		},
-	],
-	position: "-9.000,38.000",
-	orderPrice: 95,
-	priorityPrice: 19,
-};
 
 const Order = () => {
 	const {
@@ -50,7 +17,7 @@ const Order = () => {
 		orderPrice,
 		estimatedDelivery,
 		cart,
-	} = order;
+	} = useLoaderData();
 	const deliveryIn = remainingMinutes(estimatedDelivery);
 
 	return (
@@ -87,4 +54,10 @@ const Order = () => {
 	);
 };
 
-export default Order;
+const loader = async ({ params }) => {
+	const order = await getOrder(params.orderId);
+
+	return order;
+};
+
+export { Order as default, loader };
