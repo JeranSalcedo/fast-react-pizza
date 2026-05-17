3fbcd50c0dc133048f151598567d03e0a1bd28b2
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-17 14:09:44 +0800

    added geolocation

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { createOrder } from "../../services/apiRestaurant";
 import { formatCurrency } from "../../utils/helpers";
-import { getUserName } from "../user/userSlice";
+import { fetchAddress, getUser } from "../user/userSlice";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
 
 import store from "../../store";
@@ -19,7 +19,11 @@ const isValidPhone = (str) =>
 	);
 
 const CreateOrder = () => {
-	const userName = useSelector(getUserName);
+	const dispatch = useDispatch();
+
+	const { userName, status, position, address, error } = useSelector(getUser);
+	const isLoading = status === "loading";
+
 	const navigation = useNavigation();
 	const isSubmitting = navigation.state === "submitting";
 
@@ -69,16 +73,38 @@ const CreateOrder = () => {
 					</div>
 				</div>
 
-				<div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+				<div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
 					<label className="sm:basis-36">Address</label>
 					<div className="grow">
 						<input
 							className="input w-full"
 							type="text"
 							name="address"
+							defaultValue={address}
+							disabled={isLoading}
 							required
 						/>
+						{status === "error" && (
+							<p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+								{error}
+							</p>
+						)}
 					</div>
+
+					{!position.latitude && !position.longitude && (
+						<span className="absolute right-1 top-[3px] z-50 md:top-[5px]">
+							<Button
+								type="small"
+								onClick={(e) => {
+									e.preventDefault();
+									dispatch(fetchAddress());
+								}}
+								disabled={isLoading}
+							>
+								Get position
+							</Button>
+						</span>
+					)}
 				</div>
 
 				<div className="mb-12 flex items-center gap-5">
@@ -101,7 +127,17 @@ const CreateOrder = () => {
 						name="cart"
 						value={JSON.stringify(cart)}
 					/>
-					<Button disabled={isSubmitting}>
+					<input
+						type="hidden"
+						name="position"
+						value={
+							position.latitude && position.longitude
+								? `${position.latitude},${position.longitude}`
+								: ""
+						}
+					/>
+
+					<Button disabled={isSubmitting || isLoading}>
 						{isSubmitting
 							? "Placing order..."
 							: `Order now ${formatCurrency(totalPrice)}${priority ? ` (${formatCurrency(totalCartPrice)} + ${formatCurrency(priorityPrice)})` : ""}`}
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,26 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-// const getPosition = () => {
-// 	return new Promise(function (resolve, reject) {
-// 		navigator.geolocation.getCurrentPosition(resolve, reject);
-// 	});
-// };
-
-// const fetchAddress = async () => {
-// 	const positionObj = await getPosition();
-// 	const position = {
-// 		latitude: positionObj.coords.latitude,
-// 		longitude: positionObj.coords.longitude,
-// 	};
-
-// 	const addressObj = await getAddress(position);
-// 	const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
-
-// 	return { position, address };
-// };
+import { getAddress } from "../../services/apiGeocoding";
 
 const initialState = {
 	userName: "",
+	status: "idle",
+	position: {},
+	address: "",
+	error: "",
 };
 
 const slice = createSlice({
@@ -31,9 +18,47 @@ const slice = createSlice({
 			state.userName = action.payload;
 		},
 	},
+	extraReducers: (builder) =>
+		builder
+			.addCase(fetchAddress.pending, (state) => {
+				state.status = "loading";
+			})
+			.addCase(fetchAddress.fulfilled, (state, action) => {
+				state.position = action.payload.position;
+				state.address = action.payload.address;
+
+				state.status = "idle";
+			})
+			.addCase(fetchAddress.rejected, (state, action) => {
+				state.error = action.error.message;
+
+				state.status = "error";
+			}),
 });
 
-export const getUserName = (state) => state.user.userName;
+const getPosition = () => {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
+};
 
+const fetchAddress = createAsyncThunk("user/fetchAddress", async () => {
+	const positionObj = await getPosition();
+	const position = {
+		latitude: positionObj.coords.latitude,
+		longitude: positionObj.coords.longitude,
+	};
+
+	const addressObj = await getAddress(position);
+	const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+	return { position, address };
+});
+
+const getUser = (state) => state.user;
+
+const getUserName = (state) => state.user.userName;
+
+export { getUser, getUserName, fetchAddress };
 export const { updateName } = slice.actions;
 export default slice.reducer;
