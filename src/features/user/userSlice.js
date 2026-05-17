2afa1c18e4696072546a7a2461683a79d0dfc89b
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-17 08:50:37 +0800

    added redux and userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,18 +1,37 @@
-const getPosition = () => {
-	return new Promise(function (resolve, reject) {
-		navigator.geolocation.getCurrentPosition(resolve, reject);
-	});
+import { createSlice } from "@reduxjs/toolkit";
+
+// const getPosition = () => {
+// 	return new Promise(function (resolve, reject) {
+// 		navigator.geolocation.getCurrentPosition(resolve, reject);
+// 	});
+// };
+
+// const fetchAddress = async () => {
+// 	const positionObj = await getPosition();
+// 	const position = {
+// 		latitude: positionObj.coords.latitude,
+// 		longitude: positionObj.coords.longitude,
+// 	};
+
+// 	const addressObj = await getAddress(position);
+// 	const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+// 	return { position, address };
+// };
+
+const initialState = {
+	userName: "",
 };
 
-const fetchAddress = async () => {
-	const positionObj = await getPosition();
-	const position = {
-		latitude: positionObj.coords.latitude,
-		longitude: positionObj.coords.longitude,
-	};
+const slice = createSlice({
+	name: "user",
+	initialState,
+	reducers: {
+		updateName(state, action) {
+			state.userName = action.payload;
+		},
+	},
+});
 
-	const addressObj = await getAddress(position);
-	const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
-
-	return { position, address };
-};
+export const { updateName } = slice.actions;
+export default slice;
