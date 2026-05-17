fbb127c014398ba268e5cd943598a844973e0fa5
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-17 10:25:57 +0800

    added onClick handler

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const Button = ({ children, type = "primary", disabled, to }) => {
+const Button = ({ children, type = "primary", disabled, to, onClick }) => {
 	const baseStyle =
 		"inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
 
@@ -19,7 +19,7 @@ const Button = ({ children, type = "primary", disabled, to }) => {
 		);
 
 	return (
-		<button className={styles[type]} disabled={disabled}>
+		<button className={styles[type]} disabled={disabled} onClick={onClick}>
 			{children}
 		</button>
 	);
